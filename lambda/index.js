8aaf86b1809f2d3d257bce735b8613532593c00e
Jorge <jrgjit@gmail.com>
2020-11-05 00:49:20 -0600

    initialize project

---
+++ lambda/index.js
@@ -0,0 +1,404 @@
+
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require('ask-sdk-core');
+const persistence = require('./persistence');
+const interceptors = require('./interceptors');
+const logic = require('./logic');
+const constants = require('./constants');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    async handle(handlerInput) {
+        const {attributesManager, serviceClientFactory, requestEnvelope} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        const day = sessionAttributes['day'];
+        const month = sessionAttributes['month']; //MM
+        const monthName = sessionAttributes['monthName'];
+        const year = sessionAttributes['year'];
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] + '.' : '';
+
+        let speechText = handlerInput.t('WELCOME_MSG', {name: name+'.'});
+
+        if(day && monthName && year){
+            speechText = handlerInput.t('REGISTER_MSG', {name: name, day: day, month: monthName, year: year}) + handlerInput.t('HELP_MSG');
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const RegisterBirthdayIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'RegisterBirthdayIntent';
+    },
+    handle(handlerInput) {
+        const {attributesManager, requestEnvelope} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const {intent} = requestEnvelope.request;
+
+        const day = intent.slots.day.value;
+        const month = intent.slots.month.resolutions.resolutionsPerAuthority[0].values[0].value.id; //MM
+        const monthName = intent.slots.month.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+        const year = intent.slots.year.value;
+
+        sessionAttributes['day'] = day;
+        sessionAttributes['month'] = month; //MM
+        sessionAttributes['monthName'] = monthName;
+        sessionAttributes['year'] = year;
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] + '. ' : '';
+
+        const speechText = handlerInput.t('REGISTER_MSG', {name: name, day: day, month: monthName, year: year}) + handlerInput.t('HELP_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const SayBirthdayIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'SayBirthdayIntent';
+    },
+    async handle(handlerInput) {
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        const day = sessionAttributes['day'];
+        const month = sessionAttributes['month']; //MM
+        const year = sessionAttributes['year'];
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] + '. ' : '';
+        let timezone = requestAttributes['timezone'];
+
+        let speechText;
+        if(day && month && year){
+            if(!timezone){
+                //timezone = 'Europe/Madrid';  // so it works on the simulator, you should uncomment this line, replace with your time zone and comment sentence below
+                return handlerInput.responseBuilder
+                    .speak(handlerInput.t('NO_TIMEZONE_MSG'))
+                    .getResponse();
+            }
+
+            const birthdayData = logic.getBirthdayData(day, month, year, timezone);
+            speechText = handlerInput.t('DAYS_LEFT_MSG', {name: name, count: birthdayData.daysUntilBirthday});
+            speechText += handlerInput.t('WILL_TURN_MSG', {count: birthdayData.age + 1});
+            if(birthdayData.daysUntilBirthday === 0) { // it's the user's birthday!
+                speechText = handlerInput.t('GREET_MSG', {name: name});
+                speechText += handlerInput.t('NOW_TURN_MSG', {count: birthdayData.age});
+
+                const dateData = logic.getAdjustedDateData(timezone);
+                const response = await logic.fetchBirthdaysData(dateData.day, dateData.month, 5);
+
+                if(response) { // if the API call fails we just don't append today's birthdays
+                    console.log(JSON.stringify(response));
+                    const results = response.results.bindings;
+                    speechText += handlerInput.t('ALSO_TODAY_MSG');
+                    results.forEach((person, index) => {
+                        console.log(person);
+                        if(index === Object.keys(results).length - 2)
+                            speechText += person.humanLabel.value + handlerInput.t('CONJUNCTION_MSG');
+                        else
+                            speechText += person.humanLabel.value + '. '
+                    });
+                }
+            }
+            speechText += handlerInput.t('OVERWRITE_MSG');
+        } else {
+            speechText = handlerInput.t('MISSING_MSG');
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const RemindBirthdayIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'RemindBirthdayIntent';
+    },
+    async handle(handlerInput) {
+        const {attributesManager, serviceClientFactory, requestEnvelope} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const {intent} = handlerInput.requestEnvelope.request;
+
+        const day = sessionAttributes['day'];
+        const month = sessionAttributes['month'];
+        const year = sessionAttributes['year'];
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] : '';
+        let timezone = requestAttributes['timezone'];
+        const message = intent.slots.message.value;
+
+        if(intent.confirmationStatus !== 'CONFIRMED') {
+
+            return handlerInput.responseBuilder
+                .speak(handlerInput.t('CANCEL_MSG') + handlerInput.t('HELP_MSG'))
+                .reprompt(handlerInput.t('HELP_MSG'))
+                .getResponse();
+        }
+
+        let speechText;
+        if(day && month && year){
+            if(!timezone){
+                //timezone = 'Europe/Madrid';  // so it works on the simulator, you should uncomment this line, replace with your time zone and comment sentence below
+                return handlerInput.responseBuilder
+                    .speak(handlerInput.t('NO_TIMEZONE_MSG'))
+                    .getResponse();
+            }
+
+            const birthdayData = logic.getBirthdayData(day, month, year, timezone);
+            console.log("birthday")
+            console.log(birthdayData)
+
+            // let's try to create a reminder via the Reminders API
+            // don't forget to enable this permission in your skill configuratiuon (Build tab -> Permissions)
+            // or you'll get a SessionEnndedRequest with an ERROR of type INVALID_RESPONSE
+            try {
+                const {permissions} = requestEnvelope.context.System.user;
+                if(!permissions)
+                    throw { statusCode: 401, message: 'No permissions available' }; // there are zero permissions, no point in intializing the API
+                const reminderServiceClient = serviceClientFactory.getReminderManagementServiceClient();
+                // reminders are retained for 3 days after they 'remind' the customer before being deleted
+                const remindersList = await reminderServiceClient.getReminders();
+                console.log('Current reminders: ' + JSON.stringify(remindersList));
+                console.log(JSON.stringify(remindersList));
+                // delete previous reminder if present
+                const previousReminder = sessionAttributes['reminderId'];
+                if(previousReminder){
+                    await reminderServiceClient.deleteReminder(previousReminder);
+                    delete sessionAttributes['reminderId'];
+                    console.log('Deleted previous reminder with token: ' + previousReminder);
+                }
+                // create reminder structure
+                const reminder = logic.createReminderData(
+                    birthdayData.daysUntilBirthday,
+                    timezone,
+                    requestEnvelope.request.locale,
+                    message);
+                const reminderResponse = await reminderServiceClient.createReminder(reminder); // the response will include an "alertToken" which you can use to refer to this reminder
+                // save reminder id in session attributes
+                sessionAttributes['reminderId'] = reminderResponse.alertToken;
+                console.log('Reminder created with token: ' + reminderResponse.alertToken);
+                speechText = handlerInput.t('REMINDER_CREATED_MSG') + handlerInput.t('HELP_MSG');
+            } catch (error) {
+                console.log(JSON.stringify(error));
+                switch (error.statusCode) {
+                    case 401: // the user has to enable the permissions for reminders, let's attach a permissions card to the response
+                        handlerInput.responseBuilder.withAskForPermissionsConsentCard(constants.REMINDERS_PERMISSION);
+                        speechText = handlerInput.t('MISSING_PERMISSION_MSG') + handlerInput.t('HELP_MSG');
+                        break;
+                    case 403: // devices such as the simulator do not support reminder management
+                        speechText = handlerInput.t('UNSUPPORTED_DEVICE_MSG') + handlerInput.t('HELP_MSG');
+                        break;
+                    //case 405: METHOD_NOT_ALLOWED, please contact the Alexa team
+                    default:
+                        speechText = handlerInput.t('REMINDER_ERROR_MSG') + handlerInput.t('HELP_MSG');
+                }
+            }
+        } else {
+            speechText = handlerInput.t('MISSING_MSG') + handlerInput.t('HELP_MSG');
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const CelebrityBirthdaysIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'CelebrityBirthdaysIntent';
+    },
+    async handle(handlerInput) {
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes()
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] : '';
+        const {requestEnvelope, serviceClientFactory} = handlerInput;
+        let timezone = requestAttributes['timezone'];
+
+        if(!timezone){
+            //timezone = 'Europe/Madrid';  // so it works on the simulator, you should uncomment this line, replace with your time zone and comment sentence below
+            return handlerInput.responseBuilder
+                .speak(handlerInput.t('NO_TIMEZONE_MSG'))
+                .getResponse();
+        }
+        timezone = 'Europe/Madrid'
+        try {
+            // call the progressive response service
+            await logic.callDirectiveService(handlerInput, handlerInput.t('PROGRESSIVE_MSG'));
+        } catch (error) {
+            // if it fails we can continue, but the user will wait without progressive response
+            console.log("Progressive directive error : " + error);
+        }
+
+        const dateData = logic.getAdjustedDateData(timezone);
+        console.log("------------response--------------------------")
+        const response = await logic.fetchBirthdaysData(dateData.day, dateData.month, 2); //fetch 5 entries
+        
+        console.log("response--------------------------")
+
+        let speechText = handlerInput.t('API_ERROR_MSG');
+        if(response) {
+            console.log("entra response")
+            console.log(JSON.stringify(response));
+            const results = response.results.bindings;
+            speechText = handlerInput.t('CELEBRITY_BIRTHDAYS_MSG');
+            results.forEach((person, index) => {
+                console.log(person);
+                if(index === Object.keys(results).length - 2)
+                    speechText += person.humanLabel.value + handlerInput.t('CONJUNCTION_MSG');
+                else
+                    speechText += person.humanLabel.value + '. '
+            });
+        }
+        speechText += handlerInput.t('HELP_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('HELP_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] : '';
+
+        const speechText = handlerInput.t('GOODBYE_MSG', {name: name});
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+    }
+};
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('FALLBACK_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        const speechText = handlerInput.t('REFLECTOR_MSG', {intent: intentName});
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speechText = handlerInput.t('ERROR_MSG');
+
+        console.log(`~~~~ Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+// This handler acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        RegisterBirthdayIntentHandler,
+        SayBirthdayIntentHandler,
+        RemindBirthdayIntentHandler,
+        CelebrityBirthdaysIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    .addErrorHandlers(
+        ErrorHandler)
+    .addRequestInterceptors(
+        interceptors.LocalisationRequestInterceptor,
+        interceptors.LoggingRequestInterceptor,
+        interceptors.LoadAttributesRequestInterceptor,
+        interceptors.LoadNameRequestInterceptor,
+        interceptors.LoadTimezoneRequestInterceptor)
+    .addResponseInterceptors(
+        interceptors.LoggingResponseInterceptor,
+        interceptors.SaveAttributesResponseInterceptor)
+    .withPersistenceAdapter(persistence.getPersistenceAdapter())
+    .withApiClient(new Alexa.DefaultApiClient())
+    .lambda();
---
+++ lambda/localization.js
@@ -0,0 +1,41 @@
+// positive sound for birthday greeting from Alexa Sound Library
+// https://developer.amazon.com/docs/custom-skills/ask-soundlibrary.html
+const POSITIVE_SOUND = `<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_positive_response_02'/>`;
+// congratulations greeting (speechcon)
+// https://developer.amazon.com/docs/custom-skills/speechcon-reference-interjections-spanish.html
+const GREETING_SPEECHCON = `<say-as interpret-as="interjection">felicidades</say-as>`;
+const DOUBT_SPEECHCON = `<say-as interpret-as="interjection">hmm</say-as>`;
+
+module.exports = {
+    es: {
+        translation: {
+            WELCOME_MSG: 'Hola {{name}} Dime. Cuando es tu fecha de cumpleaños? ',
+            REGISTER_MSG: '{{name}} Tu fecha de cumpleaños es el {{day}} de {{month}} de {{year}}. ',
+            DAYS_LEFT_MSG: '{{name}} Queda {{count}} día ',
+            DAYS_LEFT_MSG_plural: '{{name}} Quedan {{count}} días ',
+            WILL_TURN_MSG: 'para que cumplas {{count}} año. ',
+            WILL_TURN_MSG_plural: 'para que cumplas {{count}} años. ',
+            GREET_MSG: POSITIVE_SOUND + GREETING_SPEECHCON + ' {{name}} ',
+            NOW_TURN_MSG: 'Hoy cumples {{count}} año! ',
+            NOW_TURN_MSG_plural: 'Hoy cumples {{count}} años! ',
+            MISSING_MSG: DOUBT_SPEECHCON + '. Parece que aun no me has dicho tu fecha de cumpleaños. Prueba decir, registra mi cumpleaños. o dime directamente una fecha. ',
+            OVERWRITE_MSG: 'Si quieres cambiar la fecha puedes decir, registra mi cumpleaños. o decirme directamente una fecha. ',
+            HELP_MSG: 'Puedo recordar tu cumpleaños si me dices una fecha. Y decirte cuanto falta para que cumplas. También puedo crear un recordatorio para cuando cumplas o decirte quién cumple años hoy. ',
+            GOODBYE_MSG: ['Hasta luego {{name}}! ', 'Adios {{name}}! ', 'Hasta pronto {{name}}! ', 'Nos vemos {{name}}! '],
+            REFLECTOR_MSG: 'Acabas de activar {{intent}}',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez. ',
+            ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez. ',
+            NO_TIMEZONE_MSG: 'No he podido determinar tu zona horaria. Verifica la configuración de tu dispositivo e inténtalo otra vez.',
+            REMINDER_CREATED_MSG: 'El recordatorio se ha creado con éxito. ',
+            REMINDER_ERROR_MSG: 'Ha habido un error al crear el recordatorio. ',
+            UNSUPPORTED_DEVICE_MSG: 'Este dispositivo no soporta la operación que estás intentando realizar. ',
+            CANCEL_MSG: 'Vale. Lo cancelamos. ',
+            MISSING_PERMISSION_MSG: 'Parece que no has autorizado el envío de recordatorios. Te he enviado una tarjeta a la app Alexa para que lo habilites. ',
+            API_ERROR_MSG: 'Lo siento, ha habido un problema de acceso a API externa. Por favor inténtalo otra vez. ',
+            PROGRESSIVE_MSG: 'Espera un momento mientras busco algunos cumpleaños de hoy. ',
+            CONJUNCTION_MSG: ' y ',
+            CELEBRITY_BIRTHDAYS_MSG: 'En esta fecha cumplen años: ',
+            ALSO_TODAY_MSG: 'También hoy cumplen: '
+        }
+    }
+}
